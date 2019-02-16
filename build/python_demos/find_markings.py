53a10f2c01d269ae680065cb69677ca2c32cdbbb
CSH Robotics <rvs314@protonmail.com>
2019-02-15 22:35:49 -0500

    NETWORKTABLES, billy, NETWORKTABLEES!

--- build/python_demos/find_markings.py
+++ build/python_demos/find_markings.py
@@ -2,77 +2,43 @@ import pixy
 import math
 from ctypes import *
 from pixy import *
+from networktables import NetworkTables
+from time import sleep
 
-print "Code Start"
+print "Starting networktables"
+
+NetworkTables.initialize(server='roboRIO-2875-FRC.local') # USB: 172.22.11.2
+sd = NetworkTables.getTable("data")
+
+print "Starting pixy"
 
 pixy.init()
 
-print "Pixy Initialized"
+print "Changing program"
 
 pixy.change_prog("line")
 
-print "Line Program Set"
-
 vects = VectorArray(1)
 
-DEAD = 5
+print "Loop begin"
 
-get_resolution()
-
-WIDTH = pixy.frame_width
-
-class Mark:
-	MIN_LEN = 10
-	MIN_ANG = 20
-	
-	def __init__(self, vect):
-		self.head = (vect.m_x1, vect.m_y1)
-		self.tail = (vect.m_x0, vect.m_y0)
-	
-	def __str__(self):
-		return "(" + str(self.length()) + "," + str(self.getAngle()) + ") @ " + str(self.getCent())
-		# return "<" + str(self.getX()) + "," + str(self.getY()) + "> @ " + str(self.getCent())
-
-	def length(self):
-		return math.hypot(self.getX(), self.getY())
-
-	def isValid(self):
-		return self.length() > Mark.MIN_LEN and self.getY() > self.getX()
-	
-	def getX(self):
-		return self.head[0] - self.tail[0]
-	
-	def getY(self):
-		return -self.head[1] + self.tail[1] # Negative b/c of reversed Y direction
-	
-	def getCent(self):
-		return ((self.head[0] + self.tail[0]) / 2, (self.head[1] + self.tail[1]) / 2)
-	
-	def getAngle(self):
-		x, y = self.getX(), self.getY()
-		sign = 1
-		if (x < 0):
-			sign = -1
-		return sign * int(math.degrees(math.atan(float(x) / float(y))))
-	
-	def isStraight(self):
-		return abs(self.getAngle()) < Mark.MIN_ANG
 
 while True:
 	res = line_get_main_features()
 	
-	hasVec = line_get_vectors(1, vects) != 0
-	
-	if not hasVec:
-		print "Found no vectors"
-		continue
-	
-	mark = Mark(vects[0])
-	
-	if not mark.isValid():	
-		print "Found no significant vectors"
-		continue
-		
-	angErr = -mark.getAngle()
-	posErr = 0
-	
+	hasVec = line_get_vectors(1, vects) == 1
+
+	sd.putBoolean("FD", hasVec)
+
+	if hasVec:
+		print "Found vector (" + str(vects[0].m_x1) + ", " + str(vects[0].m_y1) + ") -> (" + str(vects[0].m_x0) + ", " + str(vects[0].m_x1) + ")"
+		sd.putNumber("HX", vects[0].m_x1)
+		sd.putNumber("HY", vects[0].m_y1)
+		sd.putNumber("TX", vects[0].m_x0)
+		sd.putNumber("TY", vects[0].m_y0)
+	else:
+		print "Found no vector"
+		sd.putNumber("HX", -1)
+		sd.putNumber("HY", -1)
+		sd.putNumber("TX", -1)
+		sd.putNumber("TY", -1)
